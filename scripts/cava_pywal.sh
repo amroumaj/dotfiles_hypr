a527035f1c60ba0e6b7c61a64b41a48b56dd8e65
amroumaj <130351124+amroumaj@users.noreply.github.com>
2025-02-18 13:58:32 +0100

    Add files via upload

--- scripts/cava_pywal.sh
+++ scripts/cava_pywal.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-color1=$( awk 'match($0, /color4=\47(.*)\47/,a) { print a[1] }' ~/.cache/wal/colors.sh )
-color2=$( awk 'match($0, /color2=\47(.*)\47/,a) { print a[1] }' ~/.cache/wal/colors.sh )
-color3=$( awk 'match($0, /color1=\47(.*)\47/,a) { print a[1] }' ~/.cache/wal/colors.sh )
-color4=$( awk 'match($0, /color0=\47(.*)\47/,a) { print a[1] }' ~/.cache/wal/colors.sh )
+color1=$( awk 'match($0, /color5=\47(.*)\47/,a) { print a[1] }' ~/.cache/wal/colors.sh )
+color2=$( awk 'match($0, /color3=\47(.*)\47/,a) { print a[1] }' ~/.cache/wal/colors.sh )
+color3=$( awk 'match($0, /color2=\47(.*)\47/,a) { print a[1] }' ~/.cache/wal/colors.sh )
+color4=$( awk 'match($0, /color4=\47(.*)\47/,a) { print a[1] }' ~/.cache/wal/colors.sh )
 
 sed -e "s/%%%%%%%/$color1/" -e "s/+++++++/$color2/" -e "s/-------/$color3/" -e "s/4444444/$color4/" ~/.config/cava/config.colors > /tmp/cava_colors
 
